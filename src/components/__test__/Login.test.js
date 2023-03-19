8568a30e49b62c56aa260af9e83e08f57ed581ba
Shrutidax <shruti.sahu@data-axle.com>
2023-03-19 12:32:48 +0530

    Test Suites

--- src/components/__test__/Login.test.js
+++ src/components/__test__/Login.test.js
@@ -1,2 +1,11 @@
 import { render,screen } from "@testing-library/react";
 import Login from "../Login";
+
+describe("Test the login component", () => {
+    test("render the login form with two button", async () => {
+        render(<Login/>);
+        const buttonList = await screen.findAllByRole("button");
+        console.log(buttonList);
+        expect(buttonList).toHaveLength(2);
+    })
+})
